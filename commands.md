dc2a10874e8345b7128960a25494529ac6fc30a3
RiosGabri <grp@cesar.school>
2026-03-16 08:58:31 -0300

    Criados os models perguntas & respostas

--- commands.md
+++ commands.md
@@ -1,10 +1,17 @@
-## Aplicação de comandos 
+# Aplicação de comandos 
+
+#### Lembre de instalar as extensões Python + Django e SQLite Viewer para usar o código
+
 - `py -m venv .venv` -> Criação de novo ambiente digital
-- `.venv\scripts\activate` -> Atvar ambiente digital
-- `pip install` -r requirements.txt' -> Instalar os frameworks listados no arquivo requirements.txt
+- `.venv\scripts\activate` -> Ativar ambiente digital
+- `pip install -r requirements.txt` -> Instalar os frameworks listados no arquivo requirements.txt
 - `pip freeze` -> Listar os componentes e suas versões que foram instalados
 - `pip freeze > requirements.txt` -> Retorna a saída desse comando (pip freeze) no arquivo requirements.txt
 - `py -m django --version` -> Ver a versão dos instalados
 - `django-admin startproject project .` -> Criar uma pasta chamada project que possui arquivos do projeto
 - `py manage.py runserver` -> Inicia o servidor web
 - `py manage.py startapp nome_app` -> Inicia uma nova pasta com os arquivos do projeto/app 
+
+- `py manage.py migrate` -> Implementar os scripts criados, construção do squlite
+- `py manage.py makemigrations forum` Cria os scripts/model para a base do banco de dados
+- `py manage.py sqlmigrate forum 0001` -> Exibe os comandos sql da tabela 
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+# https://dontpad.com/fds_2b_2026.1
+
+import datetime
+from django.utils import timezone
+
+class Pergunta(models.Model):
+    titulo = models.CharField(max_length=200, null=False) #Não pode ficar vazio, texto obrigatório
+    detalhe = models.TextField(null=False) #texto obrigatório
+    tentativa = models.TextField() #texto opcional, pode ficar vazio
+    data_criacao = models.DateTimeField("Criado em ") #Data de criação
+    usuario = models.CharField(max_length=200, null=False, default="anônimo") #Nome do usuário
+
+    def __str__(self): #Retorna o título da pergunta, para facilitar a identificação
+        return "[" + str(self.id) + "] " + self.titulo
+    
+    def foi_publicado_recentemente(self): #Retorna verdadeiro se a pergunta foi criada nos últimos 24 horas
+        return self.data_criacao >= timezone.now() - datetime.timedelta(days=1)
+
+    def string_detalhada(self): #Retorna uma string detalhada com todas as informações da pergunta, para facilitar a visualização
+        return "id: " + str(self.id) + "; titulo: " + self.titulo + "; detalhe: " + self.detalhe + "; tentativa: " + self.tentativa + "; data criação: " + str(self.data_criacao) + "; usuario: " + self.usuario
+
+
+class Resposta(models.Model): #Cada resposta está associada a uma pergunta, tem um texto, um número de votos, uma data de criação e um usuário
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE) #ForeignKey para associar a resposta a uma pergunta, se a pergunta for deletada, as respostas associadas também serão deletadas
+    # Foreignt para apontar uma chave primária de outra tabela
+    # on_delete=models.CASCADE para deletar as respostas associadas quando a pergunta for deletada
+    texto = models.TextField(null=False) #texto obrigatório, não pode ficar vazio
+    votos = models.IntegerField(default=0) #Número de votos, inicia com 0, pode ser incrementado ou decrementado pelos usuários
+    data_criacao = models.DateTimeField("Criado em ") #Data de criação da resposta
+    usuario = models.CharField(max_length=200, null=False, default="anônimo") #Nome do usuário que criou a resposta, texto obrigatório, não pode ficar vazio, inicia com "anônimo" por padrão
+
+
+    def __str__(self): #Retorna o texto da resposta, para facilitar a identificação
+        return "[" + str(self.id) + "] " + self.texto
+    
+    def foi_publicado_recentemente(self): #Retorna verdadeiro se a resposta foi criada nos últimos 24 horas
+        return self.data_criacao >= timezone.now() - datetime.timedelta(days=1)
